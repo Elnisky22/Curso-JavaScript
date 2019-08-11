91f848ac1205eb8b0067deff190479c20b886010
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-08-11 14:32:29 -0300

    Módulo 4 concluído

--- Módulo 4/Exercício 3/util.js
+++ Módulo 4/Exercício 3/util.js
@@ -3,6 +3,10 @@ var listRepos = document.querySelector('#listRepos');
 function listar() {
     var user = document.querySelector('#user').value;
 
+    var item = document.createElement('li');
+    item.innerHTML = "Carregando...";
+    listRepos.appendChild(item);
+
     axios.get('https://api.github.com/users/' + user + '/repos')
         .then(function (response) {
             listRepos.innerHTML = "";
@@ -15,6 +19,12 @@ function listar() {
                 item.appendChild(name);
                 listRepos.appendChild(item);
             }
-        }
-    );
+        })
+        .catch(function () {
+            listRepos.innerHTML = "";
+            var item = document.createElement('li');
+            item.innerHTML = "Erro 404, usuário não existe";
+            listRepos.appendChild(item);
+        })
+    ;
 }
